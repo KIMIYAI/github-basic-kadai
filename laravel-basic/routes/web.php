aab3a7dcb24004efb3c370e4cb0e13c1a8315dc6
KIMIYAI <kimiya08113278@outlook.jp>
2026-05-07 10:10:32 +0900

    コミットメッセージ

--- laravel-basic/routes/web.php
+++ laravel-basic/routes/web.php
@@ -29,6 +29,10 @@ Route::get('/hello', [HelloController::class, 'index']);
 
 Route::get('/products', [ProductController::class, 'index']);
 
+Route::get('/products/create', [ProductController::class, 'create']);
+
+Route::post('/products/store', [ProductController::class, 'store'])->name('products.store');
+
 Route::get('/products/{id}', [ProductController::class, 'show']);
 
 Route::get('/requests/create', [RequestController::class, 'create']);
@@ -37,4 +41,12 @@ Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('r
 
 Route::get('/responses', [ResponseController::class, 'index']);
 
+Route::get('/vendors/create', [VendorController::class, 'create']);
+
+Route::post('/vendors/store', [VendorController::class, 'store'])->name('vendors.store');
+
 Route::get('/vendors/{id}', [VendorController::class, 'show']);
+
+Route::get('/requests/create', [RequestController::class, 'create']);
+
+Route::post('/requests/confirm', [RequestController::class, 'confirm'])->name('requests.confirm');
